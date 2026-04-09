e9f97e2115ba4302e3db6c5927c3b71c96e32a26
Aditya Shukla <adityashukla14920@gmail.com>
2026-04-09 23:47:44 +0530

    Updated the backend links

--- inference.py
+++ inference.py
@@ -52,7 +52,7 @@ load_dotenv()
 #  Configuration
 # ──────────────────────────────────────────────────────────────────────────────
 
-ENV_URL = os.getenv("ENV_URL", "https://prototype05-droneenv2.hf.space")
+ENV_URL = os.getenv("ENV_URL", "https://prototype05-droneenv.hf.space")
 API_KEY = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
 API_BASE_URL = os.getenv("API_BASE_URL") or "https://router.huggingface.co/v1"
 MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct"
